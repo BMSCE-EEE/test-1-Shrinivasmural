6b14389053f7c7db20066c583e99bddc7c60c293
shrinivas mural <94944853+Shrinivasmural@users.noreply.github.com>
2021-11-26 03:47:32 +0000

    n

--- p1original.c
+++ p1original.c
@@ -1,2 +1,12 @@
-
+#include<stdio.h>
+int main(void)
+{
+   int a,b,S;
+   printf("Enter the value of a \n");
+   scanf("%d",&a);
+   printf("Enter the value of b \n");
+   scanf("%d",&b);
+ S=a+b;
+ printf(" Sum of two numbers %d",S);
+ return 0;
 }
